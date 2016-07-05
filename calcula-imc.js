1bf4ccda469b8fc12be7191251ae66c0e619f4b1
Josélia Costa <joselialcosta@gmail.com>
2016-07-04 23:16:25 -0300

    Exercicio 5

---
+++ calcula-imc.js
@@ -0,0 +1,26 @@
+var trsPacientes = document.getElementsByClassName("paciente");
+
+for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++){
+
+    var pacienteTr = trsPacientes[posicaoAtual];
+
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+    var paciente = { nome : tdNome.textContent, peso : tdPeso.textContent, altura : tdAltura.textContent};
+
+    if(paciente.altura != 0){
+        var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+        tdImc.textContent = imc;
+
+        console.log(imc);
+
+    }else{
+
+        console("Não posso dividir por zero!");
+    }
+
+}
